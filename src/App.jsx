ab699d31a59e96b9bf8c393422d3b5f512a3b397
lakshminarayana-kodavali <50748755+lakshminarayana-kodavali@users.noreply.github.com>
2025-09-10 13:22:03 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,8 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h1>Hi This is GIT ACTION EXPERIMENT</h1>
+      <h1>Hi This is GIT ACTION DEPLOYMENT OF SIMPLE REACT PROJECT</h1>
+      <h1>BY LAKSHMINARAYANA KODAVALI</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
